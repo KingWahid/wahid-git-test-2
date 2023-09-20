788f6e8760b068c6dbbb710f6165deca7d89ae9e
KingWahid <wahidann765@gmail.com>
2023-09-20 09:13:36 +0700

    menambahkan pararagraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remote</title>
+</head>
+<body>
+    <h1>git adalah sebauh verdsin contorl yang sanagat di minati prgrammer</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, cum. Eligendi, quia. Quasi distinctio velit, ducimus numquam in animi labore tempora deleniti similique magni corrupti nulla consectetur repellat perferendis aspernatur.</p>
+</body>
+</html>
